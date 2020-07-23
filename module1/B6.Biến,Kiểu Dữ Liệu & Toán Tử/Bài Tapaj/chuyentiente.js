6b9da1f22dc0d31684a1561021c0803956dfaa63
hieuminh2403 <you@example.com>
2020-07-23 19:32:18 +0700

    Hello

---
+++ module1/B6.Biến,Kiểu Dữ Liệu & Toán Tử/Bài Tapaj/chuyentiente.js
@@ -0,0 +1,8 @@
+function convertCurrency() {
+    let amount = document.getElementById(elementd: amount  );
+    let fromCurrency = document.getElementById(elementd: fromCurrency);
+    let ToCurrency = document.getElementById(elementd: ToCurrency);
+   let amountNumber = Number(amount.value);
+   let result = document.getElementById
+
+}
